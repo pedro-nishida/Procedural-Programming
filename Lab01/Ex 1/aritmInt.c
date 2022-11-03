f1434f6ff582bc38240f43fc4bcaca4c53f3c656
PedroNishida <phfujinami@gmail.com>
2022-11-03 10:23:08 -0300

    commit exercício 1 - Lab 1

---
+++ Lab01/Ex 1/aritmInt.c
@@ -0,0 +1,28 @@
+/**
+ * Arquivo: aritmInt.c
+ * Autor: Pedro Henrique Fujinami Nishida
+ * Matrícula: 12121ECP015
+ * Criado em: 3 de nov 2022
+*/
+
+//Importação de funções entrada e saída
+#include <stdio.h>
+
+int main() {
+
+    //Declaração de variaáveis
+    int primeiroInt, segundoInt;
+
+    //Pede usuário escrever 2 inteiros para input
+    printf("Insira dois numeros inteiro: \n");
+    scanf("%d %d", &primeiroInt, &segundoInt);
+
+    //Saída de Dados
+    printf("%d + %d = %d,  ", primeiroInt, segundoInt, primeiroInt+segundoInt);
+    printf("%d - %d = %d,  ", primeiroInt, segundoInt,  primeiroInt-segundoInt);
+    printf("%d * %d = %d,  ", primeiroInt, segundoInt,  primeiroInt*segundoInt);
+    printf("%d / %d = %d,  ", primeiroInt, segundoInt,  primeiroInt/segundoInt);
+    printf("%d %% %d = %d,  ", primeiroInt, segundoInt,  primeiroInt%segundoInt);
+    
+    return 0;
+}
